2cb4ff4048a49e6e0490ccf467aea297dd7ce304
JoeyRandsdorp <75208664+JoeyRandsdorp@users.noreply.github.com>
2021-01-10 23:27:44 +0100

    Add files via upload

---
+++ admin.php
@@ -0,0 +1,18 @@
+<?php
+
+$conn = mysqli_connect()
+
+if (!$conn) {
+    die("Site niet bereikbaar " . mysqli_connect_error())
+}
+  
+  $delete = "DELETE FROM members WHERE (volledige-naam, vereniging, datum, email, telnummer)";
+  
+  if (mysqli_query($conn, $delete)) {
+    echo "Reservering succesvol verwijdert";
+  } else {
+    echo "Reserveringen verwijderen is niet geslaagd " . $conn->error;
+  }
+  
+  mysqli_close($conn);
+  ?>
