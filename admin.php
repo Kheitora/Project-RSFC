7190bb46aeb2c5a89fa052399ab1f4bbccf4f528
JoeyRandsdorp <75208664+JoeyRandsdorp@users.noreply.github.com>
2021-01-20 11:46:13 +0100

    Update admin.php

--- admin.php
+++ admin.php
@@ -16,7 +16,7 @@ if (!$conn) {
   
   mysqli_close($conn);
   ?>
-  <!doctype html>
+ <!DOCTYPE html>
 <html lang= "en">
     <head>  
         <meta charset="UTF-8">
@@ -28,10 +28,10 @@ if (!$conn) {
           table, th, td {
             border: 1px solid black
           }
-        </style>
+        </style>                             
     </head>
     <body>
-	<div id="blok2" > 
+	<div id="blok2">
              <div id="menu">
                 <nav>
 				<h3>.....</h3>
@@ -40,10 +40,11 @@ if (!$conn) {
 				<h3>.....</h3>
 				<h3>.....</h3>
                  <ul id="menulist">
-                   <li><a href="index.html"><h1>Home</h1></a></li>      
+                   <li><a href="index.html"><h1>Home</h1></a></li>   
+                   <li><a href="Inloggen.html"><h1>Inloggen</h1></a></li>   
                    <li><a href="Curriculum.html"><h1>Curriculum Vitae</h1></a></li>   
                    <li><a href="Opnames.html"><h1>Opnames</h1></a></li>   
-                   <li><a href="Reserveren.php"><h1>Reservering aanvragen</h1></a></li>   
+                   <li><a href="Reserveren.html"><h1>Reservering aanvragen</h1></a></li>   
                  </ul>
                  <ul id="Social">
                    <li><a href="#"><h2>Facebook</h2></a></li>
@@ -53,12 +54,12 @@ if (!$conn) {
                  </ul>
              </nav>   
             </div>
-            <form action="" method="select">
-            <div id="blok1" style="background-color: white;">
-
+       
+            <div id="blok1">
+			
+			</div>
+			</div>
             <?php
-
-
 
 // Create connection
 $conn = new mysqli("localhost", "root", "", "john_database");
@@ -84,8 +85,7 @@ if ($result->num_rows > 0) {
 $conn->close();
 ?>
             <div>
-			<input type="button" name="verwijder afspraak" value="Verwijder reservering">
+			<input type="hidden" name="reservering verwijderen" value="<?php echo $row["volledige_naam"];">
 			</div>
           </body>
           </html>
-		
